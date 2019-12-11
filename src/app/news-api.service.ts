678e3d3eb2ca840769ea86706ce690e53d293361
gojkovicmilos <gojkovic.milos@outlook.com>
2019-12-11 18:37:06 +0100

    added article class

--- src/app/news-api.service.ts
+++ src/app/news-api.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Article } from 'src/article';
 
 @Injectable({
   providedIn: 'root'
@@ -8,10 +9,16 @@ export class NewsApiService {
 
   constructor(private http:HttpClient) { }
 
+  articles:Article[] = [];
+
   getTopHeadlines()
   {
     this.http.get("https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=0ddacba682974e2eb952e80e315bb170").subscribe(res =>{
-      console.log(res);
+      this.articles = res['articles'];
+      this.articles.forEach(element => {
+        console.log(element.title);
+        
+      });
     });
   }
 }
---
+++ src/article.ts
@@ -0,0 +1,11 @@
+export class Article
+{
+    author:string;
+    content:string;
+    description:string;
+    published:string;
+    source:{id:string, name:string};
+    title:string;
+    url:string;
+    imgUrl:string;
+}
