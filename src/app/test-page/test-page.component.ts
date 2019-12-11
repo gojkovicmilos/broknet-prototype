a6902528fa87fd0593967ac5fb38fe9563fb811c
emilurosev <urosevemil@gmail.com>
2019-12-11 16:01:24 +0100

    partial update

--- src/app/test-page/test-page.component.ts
+++ src/app/test-page/test-page.component.ts
@@ -7,12 +7,40 @@ import { FinancialApiService } from '../financial-api.service';
   styleUrls: ['./test-page.component.css']
 })
 export class TestPageComponent implements OnInit {
+  links = ['All', 'Stock', 'Crypto', 'Forex'];
+  activeLink = this.links[0];
+  showStock = false;
+  showCrypto = false;
+  showForex = false;
 
   typed:string = "";
 
   constructor(private fs: FinancialApiService) { }
 
   ngOnInit() {
+  }
+
+  show(link: String): void {
+    switch(link) {
+      case "Stock":
+        this.showStock = true;
+        this.showCrypto = false;
+        this.showForex = false;
+      case "Forex":
+        this.showStock = false;
+        this.showCrypto = false;
+        this.showForex = true;
+      case "Crypto":
+        this.showStock = false;
+        this.showCrypto = true;
+        this.showForex = false;
+      case "All":
+        this.showStock = true;
+        this.showCrypto = true;
+        this.showForex = true;
+    }
+    console.log(link);
+      
   }
 
 
